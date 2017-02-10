387830d729771a00bdba17e5949b42a69539d026
Irene Hardjono <inat_7@yahoo.com>
2017-02-10 15:48:43 -0500

    Adding initial commit

---
+++ src/main/java/com/restfully/shop/services/ChatApplication.java
@@ -0,0 +1,23 @@
+package com.restfully.shop.services;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/services")
+public class ChatApplication extends Application
+{
+   private Set<Object> singletons = new HashSet<Object>();
+
+   public ChatApplication()
+   {
+      singletons.add(new ChatServices());
+   }
+
+   @Override
+   public Set<Object> getSingletons()
+   {
+      return singletons;
+   }
+}
